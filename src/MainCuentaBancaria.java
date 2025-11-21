8a22ec73c3374418b61580c0e6a35ce4373cc7ae
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-21 20:53:53 +0100

    Update MainCuentaBancaria.java
    introduccion de comentarios

--- src/MainCuentaBancaria.java
+++ src/MainCuentaBancaria.java
@@ -1,6 +1,7 @@
 public class MainCuentaBancaria {
     public static void main(String[] args) {
         Cuentabancaria cuenta = new Cuentabancaria(100);
+        //Creamos una excepcion try y catch para que cuando retire dinero y el saldo sea negativo salga un error
         try{
             cuenta.retirar(150);
         }catch (SaldoInsuficienteException e){
