2a07b8a4a1e6e929a681f4f0c3ad27f4fe519aff
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-22 11:18:48 +0100

    Update Cuentabancaria.java
    introduccion de datos

--- src/Cuentabancaria.java
+++ src/Cuentabancaria.java
@@ -1,15 +1,18 @@
+//Creamos la clase Cuentabancaria y la variable double
 public class Cuentabancaria {
     private double saldo;
-
+    
     public Cuentabancaria(double saldoinicial) {
         this.saldo = saldoinicial;
     }
+    //creamos una excepcion en el cual tenemos que decir que si el saldo es negarivo muestre el error
     public void retirar(double cantidad) throws SaldoInsuficienteException {
         if (cantidad > saldo) {
             throw new SaldoInsuficienteException("Saldo insuficiente para retirar " + cantidad);
         }
         saldo -= cantidad;
     }
+    //creamos el get de saldo para que nos muestre la cantidad
     public double getSaldo() {
         return saldo;
     }
